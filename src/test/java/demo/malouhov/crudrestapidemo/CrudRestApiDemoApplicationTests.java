8fc3a8c5450061bc5d9b4f4d148fe409e187b803
Ignat Malouhov <malouhov@yahoo.com>
2023-03-23 20:42:00 +0300

    Initial commit

---
+++ src/test/java/demo/malouhov/crudrestapidemo/CrudRestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.malouhov.crudrestapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
