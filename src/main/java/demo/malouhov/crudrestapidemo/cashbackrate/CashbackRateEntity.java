cc211ed81a3d665bbc4fe694cd41a83d1e19e989
Ignat Malouhov <malouhov@yahoo.com>
2023-04-02 16:51:24 +0300

    Add CashBackEntity and one-to-one mapping

---
+++ src/main/java/demo/malouhov/crudrestapidemo/cashbackrate/CashbackRateEntity.java
@@ -0,0 +1,46 @@
+package demo.malouhov.crudrestapidemo.cashbackrate;
+
+import demo.malouhov.crudrestapidemo.cashback.CashbackEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "cashback_rates")
+public class CashbackRateEntity {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @NotNull
+    @Enumerated(EnumType.ORDINAL)
+    private CashbackEntity.CashbackCategory cashbackCategory;
+
+    @NotNull
+    private BigDecimal amount;
+
+
+    public CashbackRateEntity() {
+    }
+
+    public CashbackRateEntity(CashbackEntity.CashbackCategory cashbackCategory, BigDecimal amount) {
+        this.cashbackCategory = cashbackCategory;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "CashbackRateEntity{" +
+                "id=" + id +
+                ", category='" + cashbackCategory + '\'' +
+                ", amount='" + amount + '\'' +
+                '}';
+    }
+
+}
